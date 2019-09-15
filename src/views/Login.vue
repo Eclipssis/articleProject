c3ad77435814f9890f6b534a6a58d6633fc1be82
Eclipssis <eclipssis.star@gmail.com>
2019-09-15 14:16:11 +0300

    create Login page

---
+++ src/views/Login.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="login">
+    <div class="login-body">
+      <div class="text-left">
+        <span class="logo"></span>
+      </div>
+
+      <div class="form-group">
+        <label for="login">Login</label>
+        <Input id="login" />
+      </div>
+
+      <div class="form-group">
+        <label for="password">Passwod</label>
+          <Input :type="'password'" toogle-password id="password" />
+      </div>
+
+      <div class="text-left">
+        <Button class="btn-login" @click="login">Login</Button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  components: {},
+  methods: {
+    login() {
+      console.log("LOGIN");
+
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+
+.logo {
+  display: inline-block;
+  width: 118px;
+  height: 18px;
+  margin-bottom: 60px;
+  margin-left: 120px;
+  background: url('../assets/logo-ligth.png') no-repeat;
+}
+
+.login {
+  display: flex;
+  flex-wrap: wrap;
+  height: 100vh;
+  overflow: hidden;
+  background: #222d32;
+  justify-content: center;
+  align-items: center;
+}
+
+label {
+  color: #ffffff;
+}
+
+.btn-login {
+  margin-left: 120px;
+}
+
+</style>
